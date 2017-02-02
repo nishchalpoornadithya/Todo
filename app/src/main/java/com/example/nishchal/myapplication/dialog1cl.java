a4db956e8ceeba4613ceaaf85f99023b08bb86e4
nishchalpro <nishchalpro@yahoo.in>
2017-02-02 18:06:58 +0530

    time picker added

--- app/src/main/java/com/example/nishchal/myapplication/dialog1cl.java
+++ app/src/main/java/com/example/nishchal/myapplication/dialog1cl.java
@@ -2,15 +2,19 @@ package com.example.nishchal.myapplication;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -22,33 +26,87 @@ import java.util.Locale;
 public class dialog1cl extends AppCompatActivity {
  private EditText ed;
      private EditText ed1;
+    private EditText ed2;
+    String am_pm="";
+    String cbText;
     private static final int av= 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dialog1);
-        ed=(EditText)findViewById(R.id.editText);
-        String st=ed.getText().toString();
-      ed1=(EditText)findViewById(R.id.editText2);
-        ImageButton im2=(ImageButton)findViewById(R.id.imageButton2);
-        im2.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        ed = (EditText) findViewById(R.id.editText);
+        String st = ed.getText().toString();
+        ed1 = (EditText) findViewById(R.id.editText2);
+        ed2 = (EditText) findViewById(R.id.editText3);
+        ImageButton im2 = (ImageButton) findViewById(R.id.imageButton2);
+        im2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
                 new DatePickerDialog(dialog1cl.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
 
 
-                               }});
+            }
+        });
 
-        ImageButton im=(ImageButton)findViewById(R.id.imageButton);
-        im.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
+        ImageButton im = (ImageButton) findViewById(R.id.imageButton);
+        im.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
 
                 promptspeech();
             }
         });
 
+        ImageButton im3 = (ImageButton) findViewById(R.id.imageButton3);
+        im3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                int minute = mcurrentTime.get(Calendar.MINUTE);
+
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(dialog1cl.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        if (mcurrentTime.get(Calendar.AM_PM) == Calendar.AM)
+                            am_pm = "AM";
+                        else if (mcurrentTime.get(Calendar.AM_PM) == Calendar.PM)
+                            am_pm = "PM";
+
+                        ed2.setText(selectedHour + ":" + selectedMinute +" "+am_pm);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+        CheckBox cb=(CheckBox)findViewById(R.id.checkBox);
+        if(cb.isChecked())
+        {
+            cbText="yes";
+        }
+        else
+        {
+            cbText="no";
+        }
+
+
+        Button b1=(Button)findViewById(R.id.setTaskButton);
+        b1.setOnClickListener(new View.OnClickListener(){
+                                  public void onClick(View view)
+                                  {
+                                      Intent in2=new Intent(dialog1cl.this,storage.class);
+                                      dialog1cl.this.startActivity(in2);
+
+
+                                  }
+
+                                  });
+
     }
+
 
     Calendar myCalendar = Calendar.getInstance();
     DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -66,20 +124,22 @@ public class dialog1cl extends AppCompatActivity {
     };
 
 
+
+
     public void promptspeech()
     {
         Intent i=new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Ssup!");
+        i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Name your task!");
         startActivityForResult(i,av );
-       // try {
+        try {
 
-       // }
-        //catch(ActivityNotFoundException a)
-        //{
-           // Toast.makeText(dialog1cl.this,"Sorry! Your device doesnt support speech Language",Toast.LENGTH_LONG).show();
-        //}
+        }
+        catch(ActivityNotFoundException a)
+        {
+            Toast.makeText(dialog1cl.this,"Sorry! Your device doesnt support speech Language",Toast.LENGTH_LONG).show();
+        }
     }
 
     public void onActivityResult(int request_code , int result_code, Intent i)
